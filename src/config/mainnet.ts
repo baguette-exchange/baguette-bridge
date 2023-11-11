e3379863c35c17b75cf67133b16449fb83f39bf2
Toinounet21 <79363334+Toinounet21@users.noreply.github.com>
2023-11-11 16:57:29 +0100

    Update mainnet.ts
    update import module (mainnet addr not updated)

--- src/config/mainnet.ts
+++ src/config/mainnet.ts
@@ -1,10 +1,9 @@
 import {ChainId} from '@layerzerolabs/lz-sdk';
-import {Token} from '@layerzerolabs/x-trader-joe-bridge';
+import {Token} from 'x-shibx-bridge';
 
 const tokens = [
-  new Token(ChainId.BSC, '0x371c7ec6D8039ff7933a2AA28EB827Ffe1F52f07', 18, 'JOE'),
-  new Token(ChainId.ARBITRUM, '0x371c7ec6D8039ff7933a2AA28EB827Ffe1F52f07', 18, 'JOE'),
-  new Token(ChainId.AVALANCHE, '0x6e84a6216ea6dacc71ee8e6b0a5b7322eebc0fdd', 18, 'JOE'),
+  new Token(ChainId.ARBITRUM, '0x371c7ec6D8039ff7933a2AA28EB827Ffe1F52f07', 18, 'SHIBX'),
+  new Token(ChainId.AVALANCHE, '0x6e84a6216ea6dacc71ee8e6b0a5b7322eebc0fdd', 18, 'SHIBX'),
 ];
 
 const proxy = [
