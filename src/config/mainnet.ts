9f9c3a410d1e95964bafd042c7d3c5c02c142d6f
Toinounet21 <79363334+Toinounet21@users.noreply.github.com>
2023-11-16 22:34:19 +0100

    Update mainnet.ts
    Update mainnet addresses

--- src/config/mainnet.ts
+++ src/config/mainnet.ts
@@ -2,14 +2,14 @@ import {ChainId} from '@layerzerolabs/lz-sdk';
 import {Token} from 'x-baguette-bridge';
 
 const tokens = [
-  new Token(ChainId.ARBITRUM, '0x371c7ec6D8039ff7933a2AA28EB827Ffe1F52f07', 18, 'SHIBX'),
-  new Token(ChainId.AVALANCHE, '0x6e84a6216ea6dacc71ee8e6b0a5b7322eebc0fdd', 18, 'SHIBX'),
+  new Token(ChainId.ARBITRUM, '0x440aBbf18c54b2782A4917b80a1746d3A2c2Cce1', 18, 'SHIBX'),
+  new Token(ChainId.AVALANCHE, '0x440aBbf18c54b2782A4917b80a1746d3A2c2Cce1', 18, 'SHIBX'),
 ];
 
 const proxy = [
   {
     chainId: ChainId.AVALANCHE,
-    address: '0x371c7ec6D8039ff7933a2AA28EB827Ffe1F52f07',
+    address: '0x2f05FB1519f05651078Ba35D792a9A64DEE17345',
   },
 ];
 
